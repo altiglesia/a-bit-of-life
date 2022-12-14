5d2d44edf95a560abe6a4e19f481c2221df32c51
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-12-13 21:30:07 -0500

    user auth with devise to include redux for sign-in

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import SignUpPage from "./login_signup/SignUpPage.js";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/hello")
+  //     .then((r) => r.json())
+  //     .then((data) => setCount(data.count));
+  // }, []);
 
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
-          </Route>
-          <Route path="/">
+          <Route path="/signup" element={<SignUpPage />}/>
+          {/* <Route path="/">
             <h1>Page Count: {count}</h1>
-          </Route>
+          </Route> */}
         </Switch>
       </div>
     </BrowserRouter>
