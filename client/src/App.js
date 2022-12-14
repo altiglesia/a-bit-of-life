fb68a7a847ca5610264df85f6dd812c59726bab6
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-12-14 14:56:35 -0500

    make signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,45 @@
 import './App.css';
-// import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { setUser } from "./store/user.js";
 import SignUpPage from "./login_signup/SignUpPage.js";
+import Home from "./Home.js";
 
 function App() {
-  // const [count, setCount] = useState(0);
 
-  // useEffect(() => {
-  //   fetch("/hello")
-  //     .then((r) => r.json())
-  //     .then((data) => setCount(data.count));
-  // }, []);
+  // const user = useSelector((state) => state.user);
+  
+  const dispatch = useDispatch();
+  useEffect(() => {
+
+    let token = localStorage.getItem("token");
+    if (token) {
+      fetch("/api/v1/profile", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else if (res.status === "401") {
+          throw new Error("unauthorized request");
+        }
+      })
+      .then(data => {
+        console.log(data)
+        dispatch(setUser(data))
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    } else {
+      return
+    }
+
+  }, [dispatch])
 
   return (
     // <BrowserRouter>
@@ -18,11 +47,10 @@ function App() {
         <Routes>
           <Route 
             path="/signup" 
-            element={<SignUpPage />}>
-          </Route>
-          {/* <Route path="/">
-            
-          </Route> */}
+            element={<SignUpPage />} />
+          <Route 
+            path="/"
+            element={<Home />} />
         </Routes>
       </div>
     // </BrowserRouter>
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import { configureStore } from '@reduxjs/toolkit';
-// import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store/store';
 
-// const store = configureStore(
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
