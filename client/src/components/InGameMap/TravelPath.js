adf39683da70cac21732c7afed09278eaa8e6d41
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2022-12-27 09:57:47 -0600

    fixing login form nav

--- client/src/components/InGameMap/TravelPath.js
+++ client/src/components/InGameMap/TravelPath.js
@@ -1,19 +1,33 @@
-import path_map from "../../assets/path_map.png"
+import path_map from "../../assets/path_map.png";
+import { useNavigate } from "react-router-dom";
 
 function TravelPath(){
 
-    return(
-        <div id="travelPath"
-                style={{
-                    boxSizing: "border-box",
-                    backgroundImage: `url(${path_map})`,
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "0px 0px",
-                    display: "inline-block",
-                    width: "640px",
-                    height: "320px"
-                }}
-        />
+    const navigate = useNavigate();
+
+    const goHome = () => {
+        navigate("/home")
+    }
+
+    function handleHomeClick() {
+        goHome()
+    }
+
+    return (
+        <>
+            <button onClick={handleHomeClick}>Home</button>
+            <div id="travelPath"
+                    style={{
+                        boxSizing: "border-box",
+                        backgroundImage: `url(${path_map})`,
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: "0px 0px",
+                        display: "inline-block",
+                        width: "640px",
+                        height: "320px"
+                    }}
+            />
+        </>
     )
 }
 
