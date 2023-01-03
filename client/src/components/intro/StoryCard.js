65cc1eed11ff0a985fecc93e5af585bdc7e38654
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2023-01-03 15:57:58 -0500

    style story component

--- client/src/components/intro/StoryCard.js
+++ client/src/components/intro/StoryCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import globe from "../../assets/globe.gif";
 
 function StoryCard({ storyCard, showNextStoryCard }) {
     const { content, id } = storyCard;
@@ -8,16 +7,24 @@ function StoryCard({ storyCard, showNextStoryCard }) {
         showNextStoryCard()
     }
 
-    // const storyImages = () => {
-
-    // }
-
+    const bit = require('../../assets/bit intro.jpg')
     const globe = require('../../assets/globe.gif')
+    const corporations = require('../../assets/corporations.jpg')
+
+    let storyImage;
+        if (storyCard.id === 1) {
+            storyImage = <img src={corporations} alt="corporations"/>;
+        } else if (storyCard.id === 2) {
+            storyImage = <img src={bit} alt="bit character"/>;
+        } else if (storyCard.id === 3) {
+            storyImage = <img src={globe} alt="globe"/>;
+        }
+
 
     return (
         <div id="storyCardContainer" onClick={handleClick}>
             <h3>{content}</h3>
-            <img src={globe}/>
+            {storyImage}
         </div>
     )
 }
