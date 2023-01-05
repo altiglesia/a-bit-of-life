698a8c5af336c7b01d6136c598f5d0779c082803
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2023-01-05 00:22:36 -0500

    added certificate

--- client/src/components/quiz/QuizResultPage.js
+++ client/src/components/quiz/QuizResultPage.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from 'react-router-dom';
+// import certificateImage from '../../assets/certificate.jpg';
 
 function QuizResultPage() {
 
     const accessBinaryValueResult = useSelector(state => state.rootReducer.user.profile.quiz_results)
     const binaryValueResult = accessBinaryValueResult.charAt(0).toUpperCase() + accessBinaryValueResult.slice(1);
 
+    const certificateImage = require('../../assets/certificate.jpg')
+
     return (
-        <>
-            <h2 id="quizResultContainer">Congratulations! You are a {binaryValueResult}!</h2>
+        <div id="quizResultContainer" 
+            style={{ 
+                padding: 0,
+                backgroundImage: `url(${certificateImage})`, 
+                height: "75vh",
+                width: "100vw", 
+                backgroundRepeat: "no-repeat" }}>
+            {/* <h2 id="binaryQuizResult">You are a {binaryValueResult}!</h2> */}
             <Link to ="/home">
                 <button>home</button>
             </Link>
-        </>
+        </div>
     )
 };
 
