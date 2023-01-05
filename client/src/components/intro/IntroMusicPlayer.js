ad75a204e0a2082781e6ddcd66fa49b35dc7e748
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2023-01-04 23:06:05 -0500

    added music to story component

---
+++ client/src/components/intro/IntroMusicPlayer.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import intro from "../../assets/a_bit_of_life_intro_mixdown.mp3"
+    
+    const useAudio = () => {
+      const [audio] = useState(new Audio(intro));
+      const [playing, setPlaying] = useState(false);
+    
+      const toggle = () => setPlaying(!playing);
+    
+      useEffect(() => {
+          playing ? audio.play() : audio.pause();
+        },
+        [playing]
+      );
+    
+      useEffect(() => {
+        audio.addEventListener('ended', () => setPlaying(false));
+        return () => {
+          audio.removeEventListener('ended', () => setPlaying(false));
+        };
+      }, []);
+    
+      return [playing, toggle];
+    };
+    
+    const IntroMusicPlayer = ({ intro }) => {
+      const [playing, toggle] = useAudio(intro);
+
+      const playIcon = require("../../assets/volumeOn.jpg")
+      const muteIcon = require("../../assets/mute.jpg")
+    
+      return (
+        <div>
+          <img onClick={toggle} src={playing ? muteIcon : playIcon} alt="volumeIcon" />
+        </div>
+      );
+    };
+    
+    export default IntroMusicPlayer;
