c60973c573bc1d129e3d26358c37ae28cec756a4
Alexandra Iglesia <alexxandraiglesia@gmail.com>
2023-01-25 14:05:45 -0500

    reconfigured routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: [:create, :update]
-      post '/login', to: 'auth#create'
-      get '/profile', to: 'users#profile'
-      patch '/profile', to: 'users#update'
-    end
-  end
 
+  # user auth routes
+  resources :users, only: [:create, :update]
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
+  patch '/profile', to: 'users#update'
+  # namespace :api do
+  #   namespace :v1 do
+  #   end
+  # end
+
+  # quiz questions routes
   resources :quiz_questions, only: [:index]
 
+  # submarine cable routes
   resources :submarine_cables, only: [:index]
 
+  # intro story routes
   resources :story_cards, only: [:index]
 
+  # client side routes
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
